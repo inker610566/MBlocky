11eb7aeb309578974f822582ccb0f3249d7f1255
inker610566 <inker610566@yahoo.com.tw>
2017-04-27 19:36:27 +0800

    Finish hex input interface of DebugActivity

--- app/src/main/java/com/inker/mblockly/DebugActivity.java
+++ app/src/main/java/com/inker/mblockly/DebugActivity.java
@@ -1,9 +1,18 @@
 package com.inker.mblockly;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
+import android.support.v4.text.TextUtilsCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.nio.charset.StandardCharsets;
 
 /**
  * Created by kuoin on 2017/4/27.
@@ -11,12 +20,58 @@ import android.support.v7.app.AppCompatActivity;
 
 public class DebugActivity  extends AppCompatActivity {
     private NavMenuUtil navUtil = new NavMenuUtil(this);
+    private TextView mBotOutput;
+    private EditText mBotInput;
+
+    private String[] SplitStringArray(String text) {
+        text = text.replaceAll(new String("[^0-9a-fA-F]"), "");
+        String[] ss = new String[(text.length()+1)/2];
+        int i = 0, j = 0;
+        for(; j+2 <= text.length() ; j += 2, i ++)
+            ss[i] = text.substring(j, j+2);
+        if(j+1 == text.length())
+            ss[i] = new String(new char[]{'0', text.charAt(j)});
+        return ss;
+    }
+
+    private String FormatString(String text) {
+        text = TextUtils.join(" ", SplitStringArray(text));
+        return text;
+    }
+
+    private byte[] ToByteArray(String text) {
+        String[] bs = SplitStringArray(text);
+        byte[] bytes = new byte[bs.length];
+        for(int i = 0 ; i < bs.length ; i ++)
+            bytes[i] = (byte)Integer.parseInt(bs[i], 16);
+        return bytes;
+    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_debug_layout);
         navUtil.onCreate();
+
+        mBotOutput = (TextView) findViewById(R.id.debugMbotOutput);
+        mBotInput = (EditText) findViewById(R.id.debugMbotInput);
+
+        ((Button)findViewById(R.id.debugFormatButton)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = FormatString(mBotInput.getText().toString());
+                mBotInput.setText(text);
+            }
+        });
+
+        ((Button)findViewById(R.id.debugSendButton)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                byte[] bs = ToByteArray(mBotInput.getText().toString());
+                mBotOutput.setText(new String(bs, StandardCharsets.UTF_8));
+            }
+        });
+
     }
 
     @Override
